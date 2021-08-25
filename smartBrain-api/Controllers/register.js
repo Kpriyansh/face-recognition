e65ecd21e8f9cdb1acaba676ff2499a410485398
Kpriyansh <71462800+Kpriyansh@users.noreply.github.com>
2021-08-25 19:01:47 +0530

    files added

---
+++ smartBrain-api/Controllers/register.js
@@ -0,0 +1,37 @@
+const handleRegister = (req, res, bcrypt, db) => {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    db.transaction(trx => {
+
+        trx.insert({
+            name: req.body.name,
+            email: req.body.email,
+            joined: new Date()
+
+        })
+            .into('users')
+            .returning('*')
+            .then((data) => {
+                // console.log(data[0].email);
+                return db('login')
+                    .insert({
+                        email: data[0].email,
+                        hash: hashedPassword
+                    })
+
+                    .returning('*')
+                    .then(response => {
+                        // console.log(data);
+                        // console.log(response);
+                        res.json(data[0]);
+                    })
+                    .catch(err => res.status(400).json('Invalid'))
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
+        .catch(err => res.status(400).json('Invalid'));
+}
+
+module.exports = {
+    handleRegister:handleRegister
+}
